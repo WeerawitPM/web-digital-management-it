8f7148902857639d56131f209071690e4b4381e5
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-04-24 15:32:00 +0700

    update: add request page

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,11 +1,14 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export function Providers({ children }) {
   return (
     <NextUIProvider>
-      {children}
+      <ChakraProvider>
+        {children}
+      </ChakraProvider>
     </NextUIProvider>
   )
 }
